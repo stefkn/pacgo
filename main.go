f7e5725aec2f6ac309706c88de8bc315379fcc9e
steken <stekenno@gmail.com>
2020-07-31 14:47:26 +0100

    add stuff i forgot i had lol

--- main.go
+++ main.go
@@ -1,18 +1,122 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+// Player is the player character \o/
+type Player struct {
+	row int
+	col int
+}
+
+var player Player
+var maze []string
+
+func init() {
+	cbTerm := exec.Command("/bin/stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cbreak mode terminal: %v\n", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("/bin/stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cookedTerm: %v\n", err)
+	}
+}
+
+func loadMaze() error {
+	f, err := os.Open("maze01.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+		fmt.Println("READ: " + line)
+	}
+
+	for row, line := range maze {
+		for col, char := range line {
+			switch char {
+			case 'P':
+				player = Player{row, col}
+			}
+		}
+	}
+
+	return nil
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
+}
+
+func printScreen() {
+	clearScreen()
+	for _, line := range maze {
+		fmt.Println(line)
+	}
+}
+
+func clearScreen() {
+	fmt.Printf("\x1b[2J")
+	moveCursor(0, 0)
+}
+
+func moveCursor(row, col int) {
+	fmt.Printf("\x1b[%d;%df", row+1, col+1)
+}
 
 func main() {
 	// initialize game
-
+	defer cleanup()
 	// load resources
+	err := loadMaze()
+	if err != nil {
+		return
+	}
 
 	// game loop
 	for {
 		// update screen
-
+		printScreen()
 		// process input
-
+		input, err := readInput()
+		if err != nil {
+			log.Printf("Error reading input: %v", err)
+			break
+		}
+		if input == "ESC" {
+			break
+		}
 		// process movement
 
 		// process collisions
@@ -20,7 +124,6 @@ func main() {
 		// check game over
 
 		// Temp: break infinite loop
-		fmt.Println("Hello, Pac Go!")
 		break
 
 		// repeat
